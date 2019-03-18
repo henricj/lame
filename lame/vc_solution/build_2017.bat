b8a7a9e9158725e1f7e8e0936279a8092cff7f87
Henric Jungheim <software@henric.org>
2019-03-18 01:41:33 -0700

    Update build_2017.bat to match build paths.

--- lame/vc_solution/build_2017.bat
+++ lame/vc_solution/build_2017.bat
@@ -68,12 +68,10 @@ cd "%~dp0"
 
 @title Relinking x64 for PGO instrumentation
 
-@echo Hello, world.
-
 msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGInstrument /t:BuildLink /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log lame.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGInstrument /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log lame_dll.vcxproj
+msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGInstrument /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log lame_enc_dll.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
 msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGInstrument /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log libmp3lame_dll.vcxproj
@@ -81,20 +79,20 @@ msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGInst
 
 @title Profiling x64
 
-"%~dp0..\output\x64\Release\lame.exe" -q0 -b128 "%~dp0..\testcase.wav" "%~dp0..\output\x64\Release\lame_testcase_128.mp3"
-"%~dp0..\output\x64\Release\lame.exe" -q0 -V0 "%~dp0..\testcase.wav" "%~dp0..\output\x64\Release\lame_testcase_V0.mp3"
-"%~dp0..\output\x64\Release\lame.exe" -q0 -V6 --resample 22.05 "%~dp0..\testcase.wav" "%~dp0..\output\x64\Release\lame_testcase_V6.mp3"
-"%~dp0..\output\x64\Release\lame.exe" -q0 --abr 56 -mm --resample 11.025 "%~dp0..\testcase.wav" "%~dp0..\output\x64\Release\lame_testcase_abr56.mp3"
-"%~dp0..\output\x64\Release\lame.exe" --decode "%~dp0..\testcase.wav" "%~dp0..\output\x64\Release\lame_testcase.mp3"
-"%~dp0..\output\x64\Release\lame_enc_dll_example.exe" "%~dp0..\testcase.wav"
-"%~dp0..\output\x64\Release\lame_test.exe" "%~dp0..\output\x64\Release\lame_test_random_pgo.mp3" 441000
+"%~dp0..\..\build\bin\v141\x64\Release\lame.exe" -q0 -b128 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\x64\Release\lame_testcase_128.mp3"
+"%~dp0..\..\build\bin\v141\x64\Release\lame.exe" -q0 -V0 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\x64\Release\lame_testcase_V0.mp3"
+"%~dp0..\..\build\bin\v141\x64\Release\lame.exe" -q0 -V6 --resample 22.05 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\x64\Release\lame_testcase_V6.mp3"
+"%~dp0..\..\build\bin\v141\x64\Release\lame.exe" -q0 --abr 56 -mm --resample 11.025 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\x64\Release\lame_testcase_abr56.mp3"
+"%~dp0..\..\build\bin\v141\x64\Release\lame.exe" --decode "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\x64\Release\lame_testcase.mp3"
+"%~dp0..\..\build\bin\v141\x64\Release\lame_enc_dll_example.exe" "%~dp0..\testcase.wav"
+"%~dp0..\..\build\bin\v141\x64\Release\lame_test.exe" "%~dp0..\..\build\bin\v141\x64\Release\lame_test_random_pgo.mp3" 441000
 
 @title Relinking x64 with PGO
 
 msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:BuildLink /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log lame.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log lame_dll.vcxproj
+msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log lame_enc_dll.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
 msbuild /m /p:Configuration=Release,Platform=x64,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_x64_2017.log libmp3lame_dll.vcxproj
@@ -118,7 +116,7 @@ cd "%~dp0"
 msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGInstrument /t:BuildLink /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log lame.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGInstrument /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log lame_dll.vcxproj
+msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGInstrument /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log lame_enc_dll.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
 msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGInstrument /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log libmp3lame_dll.vcxproj
@@ -126,20 +124,20 @@ msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGIn
 
 @title Profiling Win32
 
-"%~dp0..\output\Win32\Release\lame.exe" -q0 -b128 "%~dp0..\testcase.wav" "%~dp0..\output\Win32\Release\lame_testcase_128.mp3"
-"%~dp0..\output\Win32\Release\lame.exe" -q0 -V0 "%~dp0..\testcase.wav" "%~dp0..\output\Win32\Release\lame_testcase_V0.mp3"
-"%~dp0..\output\Win32\Release\lame.exe" -q0 -V6 --resample 22.05 "%~dp0..\testcase.wav" "%~dp0..\output\Win32\Release\lame_testcase_V6.mp3"
-"%~dp0..\output\Win32\Release\lame.exe" -q0 --abr 56 -mm --resample 11.025 "%~dp0..\testcase.wav" "%~dp0..\output\Win32\Release\lame_testcase_abr56.mp3"
-"%~dp0..\output\Win32\Release\lame.exe" --decode "%~dp0..\testcase.wav" "%~dp0..\output\Win32\Release\lame_testcase.mp3"
-"%~dp0..\output\Win32\Release\lame_enc_dll_example.exe" "%~dp0..\testcase.wav"
-"%~dp0..\output\Win32\Release\lame_test.exe" "%~dp0..\output\Win32\Release\lame_test_random_pgo.mp3" 441000
+"%~dp0..\..\build\bin\v141\Win32\Release\lame.exe" -q0 -b128 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\Win32\Release\lame_testcase_128.mp3"
+"%~dp0..\..\build\bin\v141\Win32\Release\lame.exe" -q0 -V0 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\Win32\Release\lame_testcase_V0.mp3"
+"%~dp0..\..\build\bin\v141\Win32\Release\lame.exe" -q0 -V6 --resample 22.05 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\Win32\Release\lame_testcase_V6.mp3"
+"%~dp0..\..\build\bin\v141\Win32\Release\lame.exe" -q0 --abr 56 -mm --resample 11.025 "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\Win32\Release\lame_testcase_abr56.mp3"
+"%~dp0..\..\build\bin\v141\Win32\Release\lame.exe" --decode "%~dp0..\testcase.wav" "%~dp0..\..\build\bin\v141\Win32\Release\lame_testcase.mp3"
+"%~dp0..\..\build\bin\v141\Win32\Release\lame_enc_dll_example.exe" "%~dp0..\testcase.wav"
+"%~dp0..\..\build\bin\v141\Win32\Release\lame_test.exe" "%~dp0..\..\build\bin\v141\Win32\Release\lame_test_random_pgo.mp3" 441000
 
 @title Relinking Win32 with PGO
 
 msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:BuildLink /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log lame.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log lame_dll.vcxproj
+msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log lame_enc_dll.vcxproj
 @if %errorlevel% neq 0 goto errorexit
 
 msbuild /m /p:Configuration=Release,Platform=Win32,WholeProgramOptimization=PGOptimize,LinkTimeCodeGeneration=PGOptimization /t:LibLinkOnly /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\lame_Win32_2017.log libmp3lame_dll.vcxproj
